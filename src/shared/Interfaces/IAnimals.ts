ea6d8f61ccf661acbf7b1d54b32f45ffcffdc6da
joaoygorr <ygorvieira2002@gmail.com>
2023-02-12 18:48:29 -0300

    feat: interface animal

---
+++ src/shared/Interfaces/IAnimals.ts
@@ -0,0 +1,10 @@
+export interface IAnimal {
+    id?: number;
+    name: string;
+    sex: string;
+    breed: string;
+    size: string;
+    weight: number;
+    type: string;
+    coat: string;
+}
