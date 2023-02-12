900b1e06a74db6ecc23ce43edc763ad10246d2e9
joaoygorr <ygorvieira2002@gmail.com>
2023-02-12 18:48:18 -0300

    feat: class animal

---
+++ src/shared/Classes/animals.class.ts
@@ -0,0 +1,21 @@
+import { IAnimal } from "../Interfaces/IAnimals";
+
+export class Animal implements IAnimal {
+    name: string;
+    sex: string;
+    breed: string;
+    size: string;
+    weight: number;
+    type: string;
+    coat: string;
+
+    constructor(name: string, sex: string, breed: string, size: string, weight: number, type: string, coat: string) {
+        this.name = name;
+        this.sex = sex;
+        this.breed = breed;
+        this.size = size;
+        this.weight = weight;
+        this.type = type;
+        this.coat = coat;
+    }
+}
