a9956507eed844788860d9228dab374314653143
joaoygorr <ygorvieira2002@gmail.com>
2023-02-15 20:19:40 -0300

    fix: remove button

--- src/shared/Components/button/button.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-    classBtn: string;
-    typeBtn: "button" | "reset" | "submit" | undefined;
-    textBtn?: string;
-}
-
-export default function Button({ classBtn, textBtn, typeBtn }: Props) {
-    return <button className={classBtn} type={typeBtn}>{textBtn}</button>
-}
