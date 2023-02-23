a70086dba6a663f4db011088fd8c18e621c37cc0
João Ygor <ygorvieira2002@gmail.com>
2023-02-23 18:21:00 -0400

    fix: remove globalStyled

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,11 @@
 import Footer from "../../shared/Components/footer/footer";
 import Header from "../../shared/Components/header/header"
 // Styleds
-import GlobalStyled from "../../Styles/global";
 
 function Home() {
 
   return (
     <div className="mainContent">
-      <GlobalStyled />
       <Header />
       
       <main>
