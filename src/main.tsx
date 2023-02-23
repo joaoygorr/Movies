fbebdcced6dea3322abe0623e1e376f7e909ea1f
João Ygor <ygorvieira2002@gmail.com>
2023-02-23 18:20:47 -0400

    fix: style global main root

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes/Router';
 import './Styles/global.css';
+import GlobalStyled from "./Styles/global";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyled />
       <Router />
     </BrowserRouter>
   </React.StrictMode>,
