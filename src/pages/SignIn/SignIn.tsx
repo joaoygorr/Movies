5f3ce4fd42ff11bc6caa65b2199d890616941352
João Ygor <ygorvieira2002@gmail.com>
2023-03-01 19:42:54 -0400

    fix: pendente ajuste

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,8 +1,33 @@
+import { useState, ChangeEvent, useEffect } from "react";
 //Styled
 import { Button } from "../../shared/Components/Button/button";
 import { BoxButton, BoxInput, BoxSignIn, ContainerSignIn } from "./SignIn.styled";
+//class 
+import { Session } from "../../shared/Classes/session.class";
+//service
+import { SessionService } from "../../shared/services/Session";
 
 const SignIn = () => {
+    const [user, setUser] = useState("");
+    const [passoword, setPassword] = useState("");
+
+    const handleChangeUser = (event: ChangeEvent<HTMLInputElement>) => {
+        setUser(event.currentTarget.value);
+    };
+
+    const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.currentTarget.value);
+    };
+
+    const handleSubmit = () => {
+        const { getToken, postSession, postSessionWithLogin } = SessionService;
+        const token = getToken()
+        console.log("🚀 ~ file: SignIn.tsx:25 ~ handleSubmit ~ token:", token)
+
+        // const loginUser = new Session(user, passoword, token);
+        // postSessionWithLogin(loginUser)
+    }
+
     return (
         <ContainerSignIn>
             <BoxSignIn className="grid grid-cols-1 grid-rows-4">
@@ -15,19 +40,31 @@ const SignIn = () => {
                 <BoxInput>
                     <label>
                         usuário*
-                        <input className="required:border-red-500 focus:ring-0" type="text" name="user" placeholder="Digite seu usuário..." />
+                        <input
+                            className="required:border-red-500 focus:ring-0"
+                            type="text"
+                            name="user"
+                            placeholder="Digite seu usuário..."
+                            onChange={(event) => handleChangeUser(event)}
+                        />
                     </label>
                 </BoxInput>
 
                 <BoxInput>
                     <label>
                         senha*
-                        <input className="required:border-red-500 focus:ring-0" type="password" name="password" placeholder="Digite sua senha..." />
+                        <input
+                            className="required:border-red-500 focus:ring-0"
+                            type="password"
+                            name="password"
+                            placeholder="Digite sua senha..."
+                            onChange={(event) => handleChangePassword(event)}
+                        />
                     </label>
                 </BoxInput>
 
                 <BoxButton>
-                    <Button typeButton="submit" textButton="entrar" classButton="bg-indigo-700" />
+                    <Button typeButton="submit" textButton="entrar" clickButton={handleSubmit} classButton="bg-indigo-700" />
                 </BoxButton>
             </BoxSignIn>
         </ContainerSignIn>
--- src/shared/services/Session.ts
+++ src/shared/services/Session.ts
@@ -1,56 +1,54 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 //api
 import { Instance } from "../../api/api";
 // interface
 import { IToken } from "../Interfaces/IToken";
 import { ISession } from "../Interfaces/ISession";
 
-function getToken() {
-    const [token, setToken] = useState<IToken>();
 
-    // useEffect(() => {
-    //     async () => {
-    //         try {
-    //             const response = await fetch(`${Instance.baseUrl}/authentication/token/new?api_key=${Instance.key}`);
-    //             const json = await response.json();
-    //             setToken(json);
-    //         } catch (error) {
-    //             console.log("🚀 ~ file: Session.ts:9 ~ useEffect ~ error:", error);
-    //         }
-    //     }
-    // }, []);
+const { baseUrl, key } = Instance;
 
-    useEffect(() => {
-        try {
-            fetch(`${Instance.baseUrl}/authentication/token/new?api_key=${Instance.key}`)
-                .then(res => res.json())
-                .then(json => setToken(json));
-        } catch (error) {
-            console.log("🚀 ~ file: Session.ts:9 ~ useEffect ~ error:", error);
-        }
-    }, []);
+const getToken = useCallback(async () => {
+    
+})
 
-    return token;
-};
 
-function postSession(data: ISession) {
+const postSession = (data: string) => {
+    const dataToken = { request_token: data };
     const requestOptions: RequestInit = {
         method: "POST",
-        headers: new Headers({ "Content-Type": "application/json" }),
-        body: JSON.parse(data)
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dataToken)
     }
 
     useEffect(() => {
         try {
-            fetch(`${Instance.baseUrl}/authentication/token/validate_with_login?api_key=${Instance.key}`, requestOptions)
+            fetch(`${baseUrl}/authentication/session/new?api_key=${key}`, requestOptions);
         } catch (error) {
-            console.log("🚀 ~ file: Session.ts:40 ~ useEffect ~ error:", error)
+            console.log("🚀 ~ file: Session.ts:40 ~ useEffect ~ error:", error);
         }
     }, [])
+}
+
+const postSessionWithLogin = (data: ISession) => {
+    const requestOptions: RequestInit = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    }
+
+    useEffect(() => {
+        try {
+            fetch(`${baseUrl}/authentication/token/validate_with_login?api_key=${key}`, requestOptions);
+        } catch (error) {
+            console.log("🚀 ~ file: Session.ts:40 ~ useEffect ~ error:", error);
+        }
+    }, []);
 }
 
 
 export const SessionService = {
     getToken,
-    postSession
+    postSession,
+    postSessionWithLogin
 }
