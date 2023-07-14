104222b3ac70a131133eb58a875f0829a41517f5
joaoygorr <ygorvieira2002@gmail.com>
2023-07-14 16:17:48 -0400

    fix: new getters

--- src/shared/services/movieService.ts
+++ src/shared/services/movieService.ts
@@ -1,14 +1,17 @@
-import { IResponse } from "../Interfaces"
-import { IMovie } from "../Interfaces/IMovie"
+import { IResponse, IMovie } from "../Interfaces"
 import { instance } from "./api/api";
 
+const getTopRated = async (): Promise<IResponse<IMovie[]>> => {
+    const topRated = await instance.get("/movie/top_rated?language=pt-br");
+    return topRated.data;
+};
 
-const getMovie = async (): Promise<IResponse<IMovie>> => {
-    const response = await instance.get("/discover/movie")
-    return response.data;
+const getPopularMovie = async (): Promise<IResponse<IMovie[]>> => {
+    const popularMovie = await instance.get("https://api.themoviedb.org/3/movie/popular?language=pt-br");
+    return popularMovie.data;
 }
 
-
 export const MovieService = {
-    getMovie
+    getTopRated,
+    getPopularMovie
 }
