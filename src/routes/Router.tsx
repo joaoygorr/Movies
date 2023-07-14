1c987472dc7ecebc3c5ae78a8cf54f895bb6e0da
joaoygorr <ygorvieira2002@gmail.com>
2023-07-14 11:39:14 -0400

    fix: comment

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,15 @@
 // config router 
 import { Routes, Route } from "react-router-dom";
-import { Auth } from "../context/auth/auth";
 // Pages 
-import SignIn from "../pages/SignIn/SignIn";
+// import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
 import Home from "../pages/Home/Home";
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/home" element={<Auth Item={<Home />} />} />
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/signin" element={<SignIn />} /> */}
             <Route path="/signup" element={<SignUp />} />
         </Routes>
     )
