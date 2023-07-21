64ce832107218c1d47c9df14f706a534d4b11f4a
joaoygorr <ygorvieira2002@gmail.com>
2023-07-21 15:07:12 -0400

    fix: finishing dialog

--- src/shared/Components/Dialog/dialogDescription.tsx
+++ src/shared/Components/Dialog/dialogDescription.tsx
@@ -1,17 +1,25 @@
+import { TextBreak } from "@/shared/utils/textBreak"
+
 type DialogDescriptionProps = {
-    description: string,
-    image: string,
+    description: string | undefined,
+    image: string | undefined,
 }
 
 export const DialogDescription = ({ description, image }: DialogDescriptionProps) => {
+    const hasDescription = description && description.trim() !== "";
+
     return (
         <div>
-            <div>
-
+            {hasDescription}
+            <div className="flex justify-center items-center">
+                <img src={"https://image.tmdb.org/t/p/w500" + image} />
             </div>
-            <div>
-                Descrição
-            </div>
+            {hasDescription && (
+                <div className="mt-2">
+                    <h3 className="font-bold text-lg font-sans border-b-2 capitalize">sinopse</h3>
+                    <p className="text-justify p-3">{TextBreak(description)}</p>
+                </div>
+            )}
         </div>
     )
 }
--- src/shared/Components/Dialog/dialogFooter.tsx
+++ src/shared/Components/Dialog/dialogFooter.tsx
@@ -1,7 +1,7 @@
 export const DialogFooter = () => {
     return (
         <div className="flex justify-center items-center">
-            <button className="bg-sky-600 p-3 rounded-full capitalize font-semibold	text-base font-sans text-white	">ver mais...</button>
+            <button className="bg-sky-600 p-3 rounded-full capitalize font-semibold	text-base font-sans text-white">ver mais...</button>
         </div>
     )
 }
