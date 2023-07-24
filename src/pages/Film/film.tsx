cd8062e3a749848fd403537a81d075c59ad18603
joaoygorr <ygorvieira2002@gmail.com>
2023-07-24 18:55:51 -0400

    fix: rename data

--- src/pages/Film/film.tsx
+++ src/pages/Film/film.tsx
@@ -3,15 +3,16 @@ import { MovieService } from "@/shared/services/movieService";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom"
 
+
 export const Film = () => {
     const { id } = useParams();
 
-    const { data } = useQuery<IDetails>("details", () => {
+    const { data: details } = useQuery<IDetails>("details", () => {
         const detail = MovieService.getDetails(parseInt(id!));
         return detail;
     }, { staleTime: 1000 * 60 })
 
     return (
-        <div>Olá</div>
+        <div></div>
     )
 }
