2f3e37e0621843b4efd3401b71fc1d8349b51719
joaoygorr <ygorvieira2002@gmail.com>
2023-07-24 16:21:05 -0400

    fix: remove language url

--- src/shared/services/movieService.ts
+++ src/shared/services/movieService.ts
@@ -2,17 +2,17 @@ import { IResponse, IMovie, IDetails } from "../Interfaces"
 import { instance } from "./api/api";
 
 const getTopRated = async (): Promise<IResponse<IMovie[]>> => {
-    const topRated = await instance.get("/movie/top_rated?language=pt-br");
+    const topRated = await instance.get("/movie/top_rated");
     return topRated.data;
 };
 
 const getPopularMovie = async (): Promise<IResponse<IMovie[]>> => {
-    const popularMovie = await instance.get("https://api.themoviedb.org/3/movie/popular?language=pt-br");
+    const popularMovie = await instance.get("https://api.themoviedb.org/3/movie/popular");
     return popularMovie.data;
 };
 
 const getDetails = async (movieId: number): Promise<IDetails> => {
-    const movie = await instance.get(`https://api.themoviedb.org/3/movie/${movieId}?language=pt-br`);
+    const movie = await instance.get(`https://api.themoviedb.org/3/movie/${movieId}`);
     return movie.data;
 }
 
