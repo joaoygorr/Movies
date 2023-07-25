173f8504999ca5d3378c3177405a9c35ce48b3cf
joaoygorr <ygorvieira2002@gmail.com>
2023-07-25 18:48:06 -0400

    fix: estrutura header film

--- src/pages/Film/film.tsx
+++ src/pages/Film/film.tsx
@@ -2,19 +2,44 @@ import { IDetails, ICredits } from "@/shared/Interfaces";
 import { CreditsService, MovieService } from "@/shared/services";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom"
-
+import { Image } from 'primereact/image';
+import "./film.style.scss";
 
 export const Film = () => {
     const { id } = useParams();
 
-    const { data: details } = useQuery<[IDetails, ICredits]>("details", () => {
-        const DetailAndCredit = Promise.all([MovieService.getDetails(parseInt(id!)), CreditsService.getCredits(parseInt(id!))])
+    const { data: detailAndCredit } = useQuery<[IDetails, ICredits]>("details", () => {
+        const DetailAndCredit = Promise.all([MovieService.getDetails(parseInt(id!)), CreditsService.getCredits(parseInt(id!))]);
         return DetailAndCredit;
     }, { staleTime: 1000 * 60 });
 
-    const [obj1, obj2] = details!;
+    const [details, credits] = detailAndCredit ? detailAndCredit : [undefined, undefined];
+
+    const year: string = details?.release_date || "";
 
     return (
-        <div></div>
+        <div className="max-w-screen-xl mx-auto box-container">
+            <div className="box-details">
+                <div className="box-image">
+                    <Image src={"https://image.tmdb.org/t/p/w500" + details?.poster_path} alt="poster filme" preview={true} />
+                </div>
+
+                <div className="box-content-details">
+                    <div className="box-title">
+                        <h1>{details?.title}</h1>
+                        <span>({year.split("-")[0]})</span>
+                    </div>
+                    
+                    <div className="box-infos">
+                        <h3 className="box-tagLine">{details?.tagline}</h3>
+
+                        <h3 className="box-sinopse">sinopse</h3>
+                        <div className="box-overview">
+                            <p>{details?.overview}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-}
+} 
