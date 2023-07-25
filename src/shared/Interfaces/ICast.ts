065acd09ed065f5ec41eb475aefb8bac5d239b35
joaoygorr <ygorvieira2002@gmail.com>
2023-07-25 11:43:51 -0400

    feat: new interface ICast

---
+++ src/shared/Interfaces/ICast.ts
@@ -0,0 +1,13 @@
+export interface ICast {
+    adult: boolean, 
+    gender: number, 
+    id: number,
+    known_for_department: string, 
+    name: string,
+    original_name: string , 
+    popularity: number, 
+    profile_pah: string, 
+    cast_id: number, 
+    credit_id: string, 
+    order: number
+}
