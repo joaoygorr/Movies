63647f77ab42796e92ae583bc40f9e0f2a69f6f2
joaoygorr <ygorvieira2002@gmail.com>
2023-07-25 11:44:10 -0400

    feat: new interface ICrew

---
+++ src/shared/Interfaces/ICrew.ts
@@ -0,0 +1,12 @@
+export interface ICrew {
+    adult: boolean,
+    gender: number,
+    known_for_department: string,
+    name: string,
+    original_name: string,
+    popularity: number,
+    profile_path: string, 
+    credit_id: string,
+    department: string, 
+    job: string 
+}
