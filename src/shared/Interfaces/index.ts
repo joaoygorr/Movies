26ce612b52b0107edcd15cff12deeb22ffe706b3
joaoygorr <ygorvieira2002@gmail.com>
2023-07-25 11:44:42 -0400

    fix: exports new interfaces

--- src/shared/Interfaces/index.ts
+++ src/shared/Interfaces/index.ts
@@ -1,3 +1,6 @@
 export { type IResponse } from "./IResponse";
 export { type IMovie } from "./IMovie";
 export { type IDetails } from "./IDetails";
+export { type ICredits } from "./iCredits";
+export { type ICrew } from "./ICrew";
+export { type ICast } from "./ICast";
