ffc75826fbc12106653253f094542a1a9dfb062f
joaoygorr <ygorvieira2002@gmail.com>
2023-07-26 18:07:14 -0400

    dix: arquivo renomeado

--- src/shared/Components/Dialog/index.tsx
+++
@@ -1,9 +0,0 @@
-import { DialogDescription } from "./dialogDescription";
-import { DialogFooter } from "./dialogFooter";
-import { DialogRoot } from "./dialogRoot";
-
-export const Dialog = {
-    Root: DialogRoot,
-    Description: DialogDescription,
-    Footer: DialogFooter
-}
