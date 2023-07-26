b9b5edc9ebf2dc53ff2f86c76397799daa2195f0
joaoygorr <ygorvieira2002@gmail.com>
2023-07-26 18:05:34 -0400

    fix: rename component

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Film } from "@/pages/Film/film";
+import { FilmDetails } from "@/pages/Film/filmDetails";
 import Home from "@/pages/Home/Home";
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/film/:id" element={<Film />} />
+            <Route path="/film/:id" element={<FilmDetails />} />
         </Routes>
     )
 }
