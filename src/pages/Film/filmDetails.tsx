776113871ccd8a06c75cd05b44d22c3ef6064810
joaoygorr <ygorvieira2002@gmail.com>
2023-07-27 19:08:55 -0400

    fix: use hook

--- src/pages/Film/filmDetails.tsx
+++ src/pages/Film/filmDetails.tsx
@@ -1,22 +1,10 @@
-import { IDetails, ICredits, IVideos } from "@/shared/Interfaces";
-import { CreditsService, MovieService } from "@/shared/services";
-import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
 import { Image } from 'primereact/image';
 import "./filmDetails.style.scss";
 import { Film } from "./components";
+import { useFilm } from "./hook/useFilm";
 
-export const FilmDetails = () => {
-    const { id } = useParams();
-    const idMovie: number = parseInt(id || '');
-
-    const { data: detailAndCredit } = useQuery<[IDetails, ICredits, IVideos]>("details", () => {
-        const DetailAndCredit = Promise.all([MovieService.getDetails(idMovie), CreditsService.getCredits(idMovie), MovieService.getVideos(idMovie)]);
-
-        return DetailAndCredit;
-    });
-
-    const [details, credits, videos] = detailAndCredit ? detailAndCredit : [undefined, undefined, undefined];
+    export const FilmDetails = () => {
+    const { credits, details, videos } = useFilm();
 
     return (
         <div className="box-container" >
