3994849ae880509835e544683f7f1255171ed200
joaoygorr <ygorvieira2002@gmail.com>
2023-07-27 18:29:11 -0400

    fix: use component footer

--- src/pages/Film/filmDetails.tsx
+++ src/pages/Film/filmDetails.tsx
@@ -1,22 +1,22 @@
-import { IDetails, ICredits } from "@/shared/Interfaces";
+import { IDetails, ICredits, IVideos } from "@/shared/Interfaces";
 import { CreditsService, MovieService } from "@/shared/services";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { Image } from 'primereact/image';
 import "./filmDetails.style.scss";
 import { Film } from "./components";
-import { ReturnHours, ReturnDate } from "@/shared/utils";
 
 export const FilmDetails = () => {
     const { id } = useParams();
+    const idMovie: number = parseInt(id || '');
 
-    const { data: detailAndCredit } = useQuery<[IDetails, ICredits]>("details", () => {
-        const DetailAndCredit = Promise.all([MovieService.getDetails(parseInt(id!)), CreditsService.getCredits(parseInt(id!))]);
+    const { data: detailAndCredit } = useQuery<[IDetails, ICredits, IVideos]>("details", () => {
+        const DetailAndCredit = Promise.all([MovieService.getDetails(idMovie), CreditsService.getCredits(idMovie), MovieService.getVideos(idMovie)]);
 
         return DetailAndCredit;
     });
 
-    const [details, credits] = detailAndCredit ? detailAndCredit : [undefined, undefined];
+    const [details, credits, videos] = detailAndCredit ? detailAndCredit : [undefined, undefined, undefined];
 
     return (
         <div className="box-container" >
@@ -25,12 +25,8 @@ export const FilmDetails = () => {
                     <Image src={"https://image.tmdb.org/t/p/w500" + details?.poster_path} alt="poster filme" preview={true} />
                 </div>
 
-                <div className="box-content-details"  >
-                    <div className="box-title">
-                        <h1>{details?.title}</h1>
-                        <span className="year">({ReturnDate(details?.release_date || "").year}) </span>
-                        <span className="hours">• {ReturnHours(details?.runtime || 0)}</span>
-                    </div>
+                <Film.Information>
+                    <Film.Header title={details?.title || ""} release_date={details?.release_date || ""} runtime={details?.runtime || 0} />
 
                     <div className="box-genres">
                         {details?.genres.map((genre, key) => {
@@ -40,15 +36,8 @@ export const FilmDetails = () => {
                         })}
                     </div>
 
-                    <div className="box-infos">
-                        <h3 className="box-tagLine">{details?.tagline}</h3>
-
-                        <h3 className="box-sinopse">sinopse</h3>
-                        <div className="box-overview">
-                            <p>{details?.overview}</p>
-                        </div>
-                    </div>
-                </div>
+                    <Film.Footer overview={details?.overview || ""} tagline={details?.tagline || ""} />
+                </Film.Information>
             </Film.Root>
         </div >
     )
