40a08755ae7eb64b26a829526b0de00af589d68d
joaoygorr <ygorvieira2002@gmail.com>
2023-07-27 17:36:34 -0400

    Update movieService.ts

--- src/shared/services/movieService.ts
+++ src/shared/services/movieService.ts
@@ -1,23 +1,17 @@
-import { IResponse, IMovie, IDetails } from "../Interfaces"
+import { IDetails, IVideos } from "../Interfaces";
 import { instance } from "./api/api";
-
-const getTopRated = async (): Promise<IResponse<IMovie[]>> => {
-    const topRated = await instance.get("/movie/top_rated");
-    return topRated.data;
-};
-
-const getPopularMovie = async (): Promise<IResponse<IMovie[]>> => {
-    const popularMovie = await instance.get("/movie/popular");
-    return popularMovie.data;
-};
 
 const getDetails = async (movieId: number): Promise<IDetails> => {
     const movie = await instance.get(`/movie/${movieId}`);
     return movie.data;
+};
+
+const getVideos = async (movieId: number): Promise<IVideos> => {
+    const video = await instance.get(`/movie/${movieId}/videos`);
+    return video.data;
 }
 
 export const MovieService = {
-    getTopRated,
-    getPopularMovie,
-    getDetails
+    getDetails,
+    getVideos
 }
