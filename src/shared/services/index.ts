605eeb9844814b3a3e4904d38f46f5659be92a4c
joaoygorr <ygorvieira2002@gmail.com>
2023-07-27 17:36:46 -0400

    fix: export service

--- src/shared/services/index.ts
+++ src/shared/services/index.ts
@@ -1,3 +1,4 @@
-export { MovieService } from "./movieService";
+export { MovieListService } from "./movieListService";
 export { CreditsService } from "./creditsService";
 export { instance } from "./api/api";
+export { MovieService } from "./movieService";
