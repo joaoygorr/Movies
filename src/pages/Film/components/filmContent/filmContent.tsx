577caf27a9db324994b3002840f0d8cf7029a2b2
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2023-12-26 21:59:27 -0300

    fix: ajuste componente film

--- src/pages/Film/components/filmContent/filmContent.tsx
+++ src/pages/Film/components/filmContent/filmContent.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./filmContent.style.scss";
+import { Dialog } from "@/shared/Components"
+import { IVideos } from "@/shared/Interfaces";
+import { useVideo } from "../../hook/useVideo";
 
 type FilmContentProps = {
     genres: [{ id: number, name: string }] | undefined
+    videos: IVideos | undefined
 }
 
-export const FilmContent = ({ genres }: FilmContentProps) => {
+export const FilmContent = ({ genres, videos }: FilmContentProps) => {
+
+    const [toggle, setToggle] = useState<boolean>(false);
+    const filteredResults = useVideo(videos);
+    const { key = '', name = '' } = filteredResults || {};
+
     return (
         <div className="box-genres">
             <div className="genre">
@@ -14,9 +24,13 @@ export const FilmContent = ({ genres }: FilmContentProps) => {
                     )
                 })}
             </div>
-            <div className="pulse">
-                <button><i className="pi pi-play" /></button>
+            <div className="pulse" onClick={() => setToggle(true)}>
+                <button type="button"><i className="pi pi-play" /></button>
             </div>
+
+            <Dialog.Root onHide={() => setToggle(false)} visible={toggle} header={name} width="auto">
+                <iframe src={`https://www.youtube.com/embed/${key}`} width={1412} height={760} allowFullScreen></iframe>
+            </Dialog.Root>
         </div>
     )
 }
