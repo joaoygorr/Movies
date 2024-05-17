f4296beee06ce5f68f182f79c48c17c94e6af896
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-05-17 18:35:25 -0300

    shore: export new interface

--- src/app/shared/interfaces/index.ts
+++ src/app/shared/interfaces/index.ts
@@ -1,6 +1,7 @@
 export { type IResponse } from "./IResponse";
 export { type IListMovie } from "./IListMovie";
 export { type IGenre } from "./IGenre";
+export { type IGenreList } from "./IGenre";
 export { type IParams } from "./IParams";
 export { type IMovie } from "./IMovie";
 export { type IVideo } from "./IVideo";
