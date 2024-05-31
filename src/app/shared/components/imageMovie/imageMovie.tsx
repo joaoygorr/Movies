99bfde3cdd2c8edde4c1cc94307645418e49e9e5
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-05-31 18:27:39 -0300

    refactor: add key

--- src/app/shared/components/imageMovie/imageMovie.tsx
+++ src/app/shared/components/imageMovie/imageMovie.tsx
@@ -10,9 +10,9 @@ export const ImageMovie = ({ param }: { param: string }) => {
             <div className="container image-box">
                 <h2>Imagens</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                    {firtNineElements?.map(e => (
-                        <div className="image">
-                            <img src={"https://image.tmdb.org/t/p/w500" + e.file_path} alt="" className="hover:opacity-75 transition ease-in-out duration-150" />
+                    {firtNineElements?.map((image, key) => (
+                        <div className="image" key={key}>
+                            <img src={"https://image.tmdb.org/t/p/w500" + image.file_path} alt="" className="hover:opacity-75 transition ease-in-out duration-150" />
                         </div>
                     ))}
                 </div>
