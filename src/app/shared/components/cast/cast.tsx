2a505e715d5147689fe59cd0cc0824e7a5624862
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-06-26 20:45:18 -0400

    style: styles refactor

--- src/app/shared/components/cast/cast.tsx
+++ src/app/shared/components/cast/cast.tsx
@@ -21,16 +21,16 @@ export const Cast = ({ param }: { param: string }) => {
         <div className="movie-cast">
             <div className="container cast-box">
                 <h2>Elenco</h2>
-                <div>
+                <div className="slider-box">
                     <Slider {...settings}>
                         {filteredImages?.map((c, k) => (
-                            <div className="mt-8" key={k}>
+                            <div className="cast" key={k}>
                                 <a href="#">
                                     <img src={"https://image.tmdb.org/t/p/w300" + c.profile_path} alt="poster elenco" className="hover:opacity-75 transition ease-in-out duration-150" />
                                 </a>
                                 <div className="mt-2">
-                                    <a href="#" className="text-lg mt-2 hover:text-gray:300">{c.original_name}</a>
-                                    <div className="text-sm text-gray-400">{c.character}</div>
+                                    <a href="#" className="hover:text-gray:300">{c.original_name}</a>
+                                    <div className="text-gray-400">{c.character}</div>
                                 </div>
                             </div>
                         ))}
--- src/app/shared/components/imageMovie/imageMovie.tsx
+++ src/app/shared/components/imageMovie/imageMovie.tsx
@@ -3,14 +3,14 @@ import "./imageMovie.style.scss";
 
 export const ImageMovie = ({ param }: { param: string }) => {
     const { data } = useImage(param);
-    const firtNineElements = data?.posters.slice(0, 9);
-
+    const filteredElements = data?.posters.slice(5, 17);
+    
     return (
         <div className="movie-image">
             <div className="container image-box">
                 <h2>Imagens</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                    {firtNineElements?.map((image, key) => (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                    {filteredElements?.map((image, key) => (
                         <div className="image" key={key}>
                             <img src={"https://image.tmdb.org/t/p/w500" + image.file_path} alt="" className="hover:opacity-75 transition ease-in-out duration-150" />
                         </div>
