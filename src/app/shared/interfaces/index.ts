4bfbb670e30e93e4b827f9ee9e1aa253c1df5813
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-08-03 12:55:41 -0400

    new interfaces

--- src/app/shared/interfaces/index.ts
+++ src/app/shared/interfaces/index.ts
@@ -1,9 +1,11 @@
 export { type IResponse } from "./IResponse";
 export { type IListMovie } from "./IListMovie";
 export { type IGenre } from "./IGenre";
-export { type IGenresResponse } from "./IGenre";
+export { type IGenresResponse } from "./IGenreResponse";
 export { type IParams } from "./IParams";
 export { type IMovie } from "./IMovie";
 export { type IVideo } from "./IVideo";
 export { type ICast } from "./ICast";
 export { type IImage } from "./IImage";
+export { type ICrew } from "./ICrew";
+export { type ICastResponse } from "./ICastResponse";
