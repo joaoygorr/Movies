1b9751c1cc5cc4a273715b4e87b92f4d22bc697b
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-08-03 16:54:06 -0400

    fix: remove log

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -12,82 +12,94 @@ import { movieApi } from "@/app/shared/api/api";
 import { useFetchData } from "@/app/shared/hook/useFetchData";
 
 export default function MovieDetails(movie: IParams) {
-  const apiCalls = useMemo(
-    () => [
-      {
-        key: "details",
-        call: () => movieApi.findByMovie(movie.params.id)
-      },
-      {
-        key: "video",
-        call: () => movieApi.findByTrailerMovie(movie.params.id, "videos")
-      }
-    ],
-    [movie.params.id]
-  );
+    const apiCalls = useMemo(
+        () => [
+            {
+                key: "details",
+                call: () => movieApi.findByMovie(movie.params.id)
+            },
+            {
+                key: "video",
+                call: () =>
+                    movieApi.findByTrailerMovie(movie.params.id, "videos")
+            }
+        ],
+        [movie.params.id]
+    );
 
-  const { data, loading } = useFetchData<{ details: IMovie; video: IVideo }>(
-    apiCalls
-  );
-  const [isVisible, setIsVisible] = useState<boolean>(false);
+    const { data, loading } = useFetchData<{ details: IMovie; video: IVideo }>(
+        apiCalls
+    );
+    const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  const formatGenres = (genres: IGenre[]) => {
-    return genres?.map((genre) => genre.name).join(" - ");
-  };
+    const formatGenres = (genres: IGenre[]) => {
+        return genres?.map((genre) => genre.name).join(" - ");
+    };
 
-  if (loading) {
-    return <Loading />;
-  }
-  console.log(data);
+    if (loading) {
+        return <Loading />;
+    }
 
-  return (
-    <div>
-      <Layout.Root>
-        <Layout.Image
-          src={"https://image.tmdb.org/t/p/w500" + data?.details?.poster_path}
-          alt="poster movie"
-          className="w-64 lg:w-96"
-        />
+    return (
+        <div>
+            <Layout.Root>
+                <Layout.Image
+                    src={
+                        "https://image.tmdb.org/t/p/w500" +
+                        data?.details?.poster_path
+                    }
+                    alt="poster movie"
+                    className="w-64 lg:w-96"
+                />
 
-        <Layout.Details>
-          <h2 className="title md:mt-0">{data?.details?.title}</h2>
-          <div className="detail-genre-date">
-            <i className="pi pi-star-fill" />
-            <span className="ml-1">
-              {Math.trunc(data?.details?.vote_average! * 10) + "%"}
-            </span>
-            <span className="mx-2">|</span>
-            <span>
-              {formatDate(new Date(data?.details?.release_date!)).modelOne}
-            </span>
-            <span className="mx-2">|</span>
-            <span>{returnHours(data?.details?.runtime!)}</span>
-            <span className="mx-2">|</span>
-            <span>{formatGenres(data?.details?.genres!)}</span>
-          </div>
-          {data?.details?.tagline && (
-            <span className="text-gray-400 mt-8">{data?.details?.tagline}</span>
-          )}
-          <p>{data?.details?.overview || "Sem Informação"}</p>
-          <div className="box-button">
-            <div>
-              {data?.video.results.length! > 0 && (
-                <button
-                  type="button"
-                  onClick={() => setIsVisible(true)}
-                  className="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
-                >
-                  <i className="pi pi-caret-right"></i>
-                  <span className="ml-2">Play Trailer</span>
-                </button>
-              )}
-            </div>
-          </div>
-        </Layout.Details>
-        {isVisible && <Modal video={data?.video!} hidden={setIsVisible} />}
-      </Layout.Root>
-      <Cast param={movie.params.id} />
-      <ImageMovie param={movie.params.id} />
-    </div>
-  );
+                <Layout.Details>
+                    <h2 className="title md:mt-0">{data?.details?.title}</h2>
+                    <div className="detail-genre-date">
+                        <i className="pi pi-star-fill" />
+                        <span className="ml-1">
+                            {Math.trunc(data?.details?.vote_average! * 10) +
+                                "%"}
+                        </span>
+                        <span className="mx-2">|</span>
+                        <span>
+                            {
+                                formatDate(
+                                    new Date(data?.details?.release_date!)
+                                ).modelOne
+                            }
+                        </span>
+                        <span className="mx-2">|</span>
+                        <span>{returnHours(data?.details?.runtime!)}</span>
+                        <span className="mx-2">|</span>
+                        <span>{formatGenres(data?.details?.genres!)}</span>
+                    </div>
+                    {data?.details?.tagline && (
+                        <span className="text-gray-400 mt-8">
+                            {data?.details?.tagline}
+                        </span>
+                    )}
+                    <p>{data?.details?.overview || "Sem Informação"}</p>
+                    <div className="box-button">
+                        <div>
+                            {data?.video.results.length! > 0 && (
+                                <button
+                                    type="button"
+                                    onClick={() => setIsVisible(true)}
+                                    className="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
+                                >
+                                    <i className="pi pi-caret-right"></i>
+                                    <span className="ml-2">Play Trailer</span>
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                </Layout.Details>
+                {isVisible && (
+                    <Modal video={data?.video!} hidden={setIsVisible} />
+                )}
+            </Layout.Root>
+            <Cast param={movie.params.id} />
+            <ImageMovie param={movie.params.id} />
+        </div>
+    );
 }
