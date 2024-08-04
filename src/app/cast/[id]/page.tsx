74bf29bcad57e1cec0e48aef90c291f0137de2b1
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-08-04 19:20:43 -0400

    fix: add new details peaple

--- src/app/cast/[id]/page.tsx
+++ src/app/cast/[id]/page.tsx
@@ -2,7 +2,8 @@
 import { castApi } from "@/app/shared/api/api";
 import { Layout } from "@/app/shared/components/layoutComponent";
 import { useFetchData } from "@/app/shared/hook/useFetchData";
-import { ICast, IParams } from "@/app/shared/interfaces";
+import { IActorDetails, IParams } from "@/app/shared/interfaces";
+import { formatDate } from "@/app/shared/utils";
 import { useMemo } from "react";
 
 export default function CastDetails(cast: IParams) {
@@ -18,9 +19,11 @@ export default function CastDetails(cast: IParams) {
         ],
         [cast.params.id]
     );
-
-    const { data } = useFetchData<{ details: ICast }>(apiCalls);
+    const { data } = useFetchData<{ details: IActorDetails }>(apiCalls);
     const details = data?.details;
+
+    const yearsOld =
+        new Date().getFullYear() - parseInt(details?.birthday.split("-")[0]!);
 
     return (
         <div>
@@ -32,7 +35,25 @@ export default function CastDetails(cast: IParams) {
                     }
                     alt="poster cast"
                     className="w-76"
-                />
+                >
+                    <ul className="info-social-media"></ul>
+                </Layout.Image>
+
+                <Layout.Details>
+                    <h2 className="text-4xl mt-4 md:mt-0 font-semibold">
+                        {details?.name}
+                    </h2>
+
+                    <div className="details-peaple text-gray-400">
+                        <i className="pi pi-gift fill-current text-gray-400 hover:text-white w-4"></i>
+                        <span className="ml-2">
+                            {formatDate(new Date(details?.birthday!)).modelOne}(
+                            {yearsOld} anos) em {details?.place_of_birth}
+                        </span>
+                    </div>
+
+                    <p className="text-gray-300 mt-8">{details?.biography}</p>
+                </Layout.Details>
             </Layout.Root>
         </div>
     );
--- src/app/shared/components/layoutComponent/imageContent.tsx
+++ src/app/shared/components/layoutComponent/imageContent.tsx
@@ -1,13 +1,22 @@
+import { ReactNode } from "react";
+
 interface ImageRootProps {
-    src: string,
-    alt: string,
-    className: string
+    src: string;
+    alt: string;
+    className: string;
+    children?: ReactNode;
 }
 
-export function ImageContent({ alt, className, src }: ImageRootProps) {
+export function ImageContent({
+    alt,
+    className,
+    src,
+    children
+}: ImageRootProps) {
     return (
         <div className="flex-none info-image">
             <img src={src} alt={alt} className={className} />
+            {children}
         </div>
-    )
+    );
 }
