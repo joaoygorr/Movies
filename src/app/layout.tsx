6e4cc65dfb505e0dd3054b5a5cda2446a5daf332
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-08-15 19:38:29 -0400

    fix: add style lazeLoad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
 import { Header } from "./shared/components/header/header";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import 'primeicons/primeicons.css';
+import "primeicons/primeicons.css";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 export const metadata: Metadata = {
-  title: "Movie",
+    title: "Movie"
 };
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-br">
-      <body>
-        <ToastContainer />
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt-br">
+            <body>
+                <ToastContainer />
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
