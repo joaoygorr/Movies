eb7606b9c59d77df0072aea6cf149bf2c94d7168
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-11-16 19:53:13 -0400

    refactor: componente modal reutilizavel

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -93,10 +93,26 @@ export default function MovieDetails(movie: IParams) {
                     </div>
                 </Layout.Details>
                 {isVisible && (
-                    <Modal
-                        video={data?.details?.videos!}
-                        hidden={setIsVisible}
-                    />
+                    <Modal>
+                        <div className="button-close">
+                            <button
+                                type="button"
+                                onClick={() => setIsVisible(false)}
+                                className="hover:text-gray-300"
+                            >
+                                x
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="modal-children responsive-container">
+                                <iframe
+                                    src={`https://www.youtube.com/embed/${data?.details?.videos.results[0].key}?autoplay=1`}
+                                    allow="autoplay; encrypted-media"
+                                    allowFullScreen
+                                />
+                            </div>
+                        </div>
+                    </Modal>
                 )}
             </Layout.Root>
             <Actors param={movie.params.id} />
--- src/app/shared/components/modal/modal.tsx
+++ src/app/shared/components/modal/modal.tsx
@@ -1,38 +1,15 @@
-import { IVideo } from "../../interfaces";
+import { ReactNode } from "react";
 import "./modal.styled.scss";
 
-export const Modal = ({
-    video,
-    hidden
-}: {
-    video: IVideo;
-    hidden: (visible: boolean) => any;
-}) => {
-    console.log("video:", video);
+interface ModalProps {
+    children: ReactNode;
+}
 
+export const Modal = ({ children }: ModalProps) => {
     return (
         <div className="modal-box shadow-lg">
             <div className="container modal lg:px-32 rounded-lg">
-                <div className="children">
-                    <div className="button-close">
-                        <button
-                            type="button"
-                            onClick={() => hidden(false)}
-                            className="hover:text-gray-300"
-                        >
-                            x
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="modal-children responsive-container">
-                            <iframe
-                                src={`https://www.youtube.com/embed/${video.results[0].key}?autoplay=1`}
-                                allow="autoplay; encrypted-media"
-                                allowFullScreen
-                            />
-                        </div>
-                    </div>
-                </div>
+                <div className="children">{children}</div>
             </div>
         </div>
     );
