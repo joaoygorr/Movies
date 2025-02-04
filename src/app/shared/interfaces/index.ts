ae60d6ad1140c53e7fac3e6ddf032b6084dfbadb
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-02-03 21:37:25 -0400

    chore:  remove IParam

--- src/app/shared/interfaces/index.ts
+++ src/app/shared/interfaces/index.ts
@@ -2,7 +2,6 @@ export { type IResponse } from "./IResponse";
 export { type IListMovie } from "./IListMovie";
 export { type IGenre } from "./IGenre";
 export { type IGenresResponse } from "./IGenreResponse";
-export { type IParams } from "./IParams";
 export { type IMovie } from "./IMovie";
 export { type IVideo } from "./IVideo";
 export { type ICast } from "./ICast";
