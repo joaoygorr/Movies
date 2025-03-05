ecd7933a7a1d912a4cdee31feed955ee9f68dd8f
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-03-05 11:49:44 -0400

    chore: remove component

--- src/shared/components/skeletonLoading/index.ts
+++ src/shared/components/skeletonLoading/index.ts
@@ -1,3 +1,2 @@
-export { default as SkeletonMain } from "./skeletonMain";
 export { default as SkeletonDetails } from "./skeletonDetails";
 export { default as SkeletonDetailsActors } from "./skeletonDetailsActors";
